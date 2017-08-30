f56c093a329a1875a3a9c2acb97c5c2328fa46d3
Jesus Equihua <jesus.equihua@improntadsi.com.mx>
2017-08-30 14:39:53 -0500

    plantilla

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<!-- Login -->
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Iniciar sesión</title>
+    <link href="lib/assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="lib/assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="lib/assets/css/custom-styles.css" rel="stylesheet" />
+</head>
+<body>
+    <div id="container">       
+        <div class="row">
+             
+        </div>
+    </div>
+    <script src="lib/assets/js/jquery-1.10.2.js"></script>
+    <script src="lib/assets/js/bootstrap.min.js"></script>
+    <script src="lib/assets/js/jquery.metisMenu.js"></script>
+    <script src="lib/assets/js/custom-scripts.js"></script>
+</body>
+</html>
