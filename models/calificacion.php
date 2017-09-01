1ba324ed19d12331322b9c0d631bf3ffab641a18
maickol12 <rocm960107@outlook.com>
2017-08-31 19:22:15 -0500

    pdf

--- models/calificacion.php
+++ models/calificacion.php
@@ -1,1 +1,1 @@
-<?php    class calificacion extends Illuminate\Database\Eloquent\Model{        protected $table = 'calificacion';        public $timestamps = false;    }?>
+<?php    class calificacion extends Illuminate\Database\Eloquent\Model{        protected $table = 'calificacion';        public $timestamps = false;        public function getCandidata(){            return $this->belongsTo('candidata','id_candidata','idcandidata');        }        public function juez()        {            return $this->hasOne('c_juez','idjuez','id_juez');        }    }?>
